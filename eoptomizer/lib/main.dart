6537425304d838ea9970a7863ab8c25f216e1c51
el3oss19 <68297539+el3oss19@users.noreply.github.com>
2022-07-01 18:56:47 +0100

    Add files via upload

--- eoptomizer/lib/main.dart
+++ eoptomizer/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,17 +18,33 @@ void main() {
   ));
 }
 
-class CustomFAB extends StatelessWidget {
+class CustomFAB extends StatefulWidget {
+  static const frequencyChannel =
+      MethodChannel('com.somdipdey.eoptomizer/frequency');
+
+  @override
+  _CustomFABState createState() => _CustomFABState();
+}
+
+class _CustomFABState extends State<CustomFAB> {
+  String frequencyLevel = 'Waiting...';
+
   Widget build(BuildContext context) {
     return FloatingActionButton.extended(
         onPressed: () {
           showSnackBar(context);
+
           print('Optimizing battery and thermal behaviour');
+          setFrequencyLevel();
         },
         backgroundColor: Colors.deepPurple[900],
         icon: Icon(Icons.thumb_up),
         label: const Text('Optimize'),
         tooltip: 'Start optimizing battery and thermal behaviour');
+  }
+
+  Future setFrequencyLevel() async {
+    await CustomFAB.frequencyChannel.invokeMethod('setFrequencyLevel');
   }
 }
 
