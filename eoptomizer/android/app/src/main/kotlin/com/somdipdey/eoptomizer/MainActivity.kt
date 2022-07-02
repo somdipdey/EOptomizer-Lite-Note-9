7890d3e85760253274f8b77e4ad8834129d8cc20
Somdip Dey <somdip007@gmail.com>
2022-07-02 18:57:24 +0100

    added text box and button to optimize

--- eoptomizer/android/app/src/main/kotlin/com/somdipdey/eoptomizer/MainActivity.kt
+++ eoptomizer/android/app/src/main/kotlin/com/somdipdey/eoptomizer/MainActivity.kt
@@ -1,6 +1,62 @@
 package com.somdipdey.eoptomizer
-
+import android.content.*
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.content.Intent
+import java.lang.Compiler.command
+
+
+
 
 class MainActivity: FlutterActivity() {
+
+
+    private val BATTERY_CHANNEL = "com.somdipdey.eoptomizer/frequency"
+    private lateinit var channel: MethodChannel
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, BATTERY_CHANNEL)
+        channel.setMethodCallHandler { call, result ->
+            if (call.method == "setFrequencyLevelCPU0Max" || call.method == "setFrequencyLevelCPU4Max" || call.method == "setFrequencyLevelCPU6Max" || call.method == "setFrequencyLevelCPU0Min" || call.method == "setFrequencyLevelCPU4Min" || call.method == "setFrequencyLevelCPU6Min") {
+                setFrequencyLevelCPU0Max()
+                setFrequencyLevelCPU4Max()
+                setFrequencyLevelCPU6Max()
+                setFrequencyLevelCPU0Min()
+                setFrequencyLevelCPU4Min()
+                setFrequencyLevelCPU6Min()
+            }
+
+        }
+    }
+    fun setFrequencyLevelCPU0Max(): Unit {
+        val process: java.lang.Process = java.lang.Runtime.getRuntime().exec("su -c echo 1598000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq")
+        process.waitFor()
+    }
+    fun setFrequencyLevelCPU4Max(): Unit {
+        val process: java.lang.Process = java.lang.Runtime.getRuntime().exec("su -c echo 1663000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq")
+        process.waitFor()
+    }
+    fun setFrequencyLevelCPU6Max(): Unit {
+        val process: java.lang.Process = java.lang.Runtime.getRuntime().exec("su -c echo 1745000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq")
+        process.waitFor()
+    }
+    fun setFrequencyLevelCPU0Min(): Unit {
+        val process: java.lang.Process = java.lang.Runtime.getRuntime().exec("su -c echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq")
+        process.waitFor()
+    }
+    fun setFrequencyLevelCPU4Min(): Unit {
+        val process: java.lang.Process = java.lang.Runtime.getRuntime().exec("su -c echo 400000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq")
+        process.waitFor()
+    }
+    fun setFrequencyLevelCPU6Min(): Unit {
+        val process: java.lang.Process = java.lang.Runtime.getRuntime().exec("su -c echo 500000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq")
+        process.waitFor()
+    }
+
 }
--- eoptomizer/lib/main.dart
+++ eoptomizer/lib/main.dart
@@ -1,31 +1,116 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.deepPurple[500],
-      appBar: AppBar(
-        title: Text('EOptimizer'),
-        backgroundColor: Colors.deepPurple[600],
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'EOptomizer',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      floatingActionButton: CustomFAB(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    ),
-  ));
+      home: new MyHomePage(title: 'EOptomizer'),
+    );
+  }
 }
 
-class CustomFAB extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const frequencyChannel =
+      MethodChannel('com.somdipdey.eoptomizer/frequency');
+  bool optimizing = false;
+  String bodyText = 'Not Optimizing';
+
+  void _optimize() {
+    setState(() {
+      if (!optimizing) {
+        print('Optimizing battery and thermal behaviour');
+        setFrequencyLevelCPU0Max();
+        setFrequencyLevelCPU4Max();
+        setFrequencyLevelCPU6Max();
+        setFrequencyLevelCPU0Min();
+        setFrequencyLevelCPU4Min();
+        setFrequencyLevelCPU6Min();
+        //showSnackBar(context);
+        optimizing = true;
+        bodyText = 'Optimizing';
+      } else {
+        optimizing = false;
+        bodyText = 'Not Optimizing';
+        print('Not optimizing battery and thermal behaviour');
+      }
+    });
+  }
+
+  Future setFrequencyLevelCPU0Max() async {
+    await frequencyChannel.invokeMethod('setFrequencyLevelCPU0Max');
+    print('Optimizing CPU0 Max Frequency');
+  }
+
+  Future setFrequencyLevelCPU4Max() async {
+    await frequencyChannel.invokeMethod('setFrequencyLevelCPU4Max');
+    print('Optimizing CPU4 Max Frequency');
+  }
+
+  Future setFrequencyLevelCPU6Max() async {
+    await frequencyChannel.invokeMethod('setFrequencyLevelCPU6Max');
+    print('Optimizing CPU6 Max Frequency');
+  }
+
+  Future setFrequencyLevelCPU0Min() async {
+    await frequencyChannel.invokeMethod('setFrequencyLevelCPU0Min');
+    print('Optimizing CPU0 Min Frequency');
+  }
+
+  Future setFrequencyLevelCPU4Min() async {
+    await frequencyChannel.invokeMethod('setFrequencyLevelCPU4Min');
+    print('Optimizing CPU4 Min Frequency');
+  }
+
+  Future setFrequencyLevelCPU6Min() async {
+    await frequencyChannel.invokeMethod('setFrequencyLevelCPU6Min');
+    print('Optimizing CPU6 Min Frequency');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-        onPressed: () {
-          showSnackBar(context);
-          print('Optimizing battery and thermal behaviour');
-        },
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+        backgroundColor: Colors.deepPurple[600],
+      ),
+      backgroundColor: Colors.deepPurple[500],
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              '$bodyText',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _optimize,
+        tooltip: 'Start optimizing battery and thermal behaviour',
+        child: new Icon(Icons.thumb_up),
         backgroundColor: Colors.deepPurple[900],
-        icon: Icon(Icons.thumb_up),
-        label: const Text('Optimize'),
-        tooltip: 'Start optimizing battery and thermal behaviour');
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
 
